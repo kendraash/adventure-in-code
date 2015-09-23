e1ec5a22b2a5f0b890ff58acf8c730ccef320e04
Kendra Ash & Rachael Berecka <student@epicodus.com>
2015-09-23 11:51:52 -0700

    Connected to firebase and made each post a link to an individual post page from the index

---
+++ app/models/post.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  date: DS.attr(),
+  author: DS.attr(),
+  title: DS.attr(),
+  text: DS.attr(),
+  image: DS.attr()  
+});
--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'adventures-in-code',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://adventure-in-code.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
